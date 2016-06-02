1797ea6c212702fcc402b75ff19275fdba99da58
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2016-06-02 01:27:53 -0300

    add cprofile example.

---
+++ web/cprof.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+from flask import Flask, request
+from werkzeug.contrib.profiler import ProfilerMiddleware
+
+app = Flask(__name__)
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, profile_dir="profile")
+app.debug = True
+
+
+@app.route("/")
+def cprof():
+    resp = requests.get("http://mtgjson.com/json/AllSetsArray.json")
+    collections = resp.json()
+    cards_name = []
+    for collection in collections:
+        for card in collection["cards"]:
+            cards_name.append(card["name"])
+    return json.dumps(cards_name)
+
+
+if __name__ == '__main__':
+    app.run(processes=4)
